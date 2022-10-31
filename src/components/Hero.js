e1aa3a4c4e94abfb10c3b8fe99781a793e785b12
Ashu131995 <ashishnaik1995@gmail.com>
2022-10-31 14:06:55 +0530

    my 2nd react project

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import react from "react"
+import grid from "../images/photo-grid.png"
+
+export default function Hero(){
+    return(
+        <section className="hero">
+            <img src={grid} className="hero--photo"/>
+            <h1 className="hero--headder">Online Experience</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
